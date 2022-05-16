93b6dcea56f81b69bae7f447d444a2e4dbe3b6e1
dholi <comunidade@dholi.dev>
2022-05-16 12:06:27 -0300

    Magento 2.4.4 support

--- src/module-payu-mapped-attributes/Helper/Data.php
+++ src/module-payu-mapped-attributes/Helper/Data.php
@@ -5,8 +5,8 @@
 * 
 * @category     elOOm
 * @package      Modulo PayUMappedAttributes
-* @copyright    Copyright (c) 2021 Ã©lOOm (https://eloom.tech)
-* @version      1.0.0
+* @copyright    Copyright (c) 2022 elOOm (https://eloom.tech)
+* @version      2.0.0
 * @license      https://eloom.tech/license
 *
 */
